ed8cabd67e6bfffc6e7a5065fc362d357be068fe
appdev-cts <cts.workmail@gmail.com>
2024-08-07 12:57:20 +0530

    test file changes

--- index.test.js
+++ index.test.js
@@ -2,9 +2,10 @@ const request = require('supertest');
 const app = require('./index');
 
 describe('GET /', () => {
-  it('should return a 404 status code', async () => {
+  it('should return a 200 status code', async () => {
     const response = await request(app).get('/');
-    expect(response.statusCode).toBe(404);
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe("Successfully running");
   });
 });
 
